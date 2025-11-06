f02af18e692a9a493b6b269afff07b4e5f14025d
arrifat346afs <arrifat1235@gmail.com>
2025-11-06 17:41:10 +0600

    feat(thumbnail): Add border to empty thumbnail section

--- src/app/app-component/thumbnail/ThumbnailSection.tsx
+++ src/app/app-component/thumbnail/ThumbnailSection.tsx
@@ -242,7 +242,7 @@ const ThumbnailSection = ({ onSelectFile }: ThumbnailSectionProps) => {
   return (
     <div className="w-">
       {(!files || files.length === 0) && (
-        <div className="h-[28vh]">
+        <div className="h-[28vh] border">
 
             <MdOutlineImageNotSupported />
           
