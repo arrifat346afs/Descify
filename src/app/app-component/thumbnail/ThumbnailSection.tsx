061230d420c20c2691b4df61b64d1192fc8aea1a
arrifat346afs <arrifat1235@gmail.com>
2025-11-06 17:42:18 +0600

    fix(thumbnail): Adjust default thumbnail size The default thumbnail size was too large, so it has been reduced.

--- src/app/app-component/thumbnail/ThumbnailSection.tsx
+++ src/app/app-component/thumbnail/ThumbnailSection.tsx
@@ -242,7 +242,7 @@ const ThumbnailSection = ({ onSelectFile }: ThumbnailSectionProps) => {
   return (
     <div className="w-">
       {(!files || files.length === 0) && (
-      <div className="h-[28vh] w-[30vw] border">
+      <div className="h-[28vh] w-[25vw] border">
 
             <MdOutlineImageNotSupported />
           
