26b02f05600cd7c51412cedf05ea754358c60495
arrifat346afs <arrifat1235@gmail.com>
2025-11-06 17:44:31 +0600

    feat(thumbnail): Add placeholder padding when no files exist

--- src/app/app-component/thumbnail/ThumbnailSection.tsx
+++ src/app/app-component/thumbnail/ThumbnailSection.tsx
@@ -242,7 +242,7 @@ const ThumbnailSection = ({ onSelectFile }: ThumbnailSectionProps) => {
   return (
     <div className="w-">
       {(!files || files.length === 0) && (
-        <div>
+        <div className="p-2">
           <div className="h-[20vh] w-[20vw] border-2 rounded-md">
             <MdOutlineImageNotSupported />
           </div>
