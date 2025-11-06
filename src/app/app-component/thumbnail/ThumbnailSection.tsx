28fdc9013372f869306482c09abab54d8b9dc8ae
arrifat346afs <arrifat1235@gmail.com>
2025-11-06 17:41:19 +0600

    fix(thumbnail): add width to empty thumbnail container The empty thumbnail container was missing a width, causing layout issues. This commit adds `w-` to ensure it occupies the full width.

--- src/app/app-component/thumbnail/ThumbnailSection.tsx
+++ src/app/app-component/thumbnail/ThumbnailSection.tsx
@@ -242,7 +242,7 @@ const ThumbnailSection = ({ onSelectFile }: ThumbnailSectionProps) => {
   return (
     <div className="w-">
       {(!files || files.length === 0) && (
-        <div className="h-[28vh] border">
+        <div className="h-[28vh] w- border">
 
             <MdOutlineImageNotSupported />
           
