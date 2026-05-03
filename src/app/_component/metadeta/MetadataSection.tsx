8c6ed6f5a6eb97058647133a27e9f5046c7e51d0
arrifat346afs <arrifat1235@gmail.com>
2026-05-03 21:14:06 +0600

    feat(ActionsSection): rename onFilesSelected to handleFilesSelected and remove padding style(MetadataSection): add orange background to ScrollArea

--- src/app/_component/metadeta/MetadataSection.tsx
+++ src/app/_component/metadeta/MetadataSection.tsx
@@ -147,7 +147,7 @@ export const MetadataSection = () => {
   };
 
   return (
-    <ScrollArea className="h-full w-full">
+    <ScrollArea className="h-full w-full bg-orange-700">
       <div className="flex flex-col gap-4 p-2 h-full">
         <TitleField />
         <DescriptionField />
