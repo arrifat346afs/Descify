a2ceb8cedefd15064a2cad84422973a35b11430a
arrifat346afs <arrifat1235@gmail.com>
2026-05-04 11:24:59 +0600

    style(ActionsSection): Add 2xl padding to component container

--- src/app/_component/action/ ActionsSection.tsx
+++ src/app/_component/action/ ActionsSection.tsx
@@ -17,7 +17,7 @@ export const  ActionsSection = ({ onFilesSelected }: ActionsSectionProps) => {
     onFilesSelected(files);
   };
   return (
-    <div className="flex justify-center items-center  select-none p-0">
+    <div className="flex justify-center items-center  select-none p-0 2xl:">
       <UploadButton onFilesSelected={handleFilesSelected}/>
       <Separator orientation="vertical" />
       <GenerateButton />
